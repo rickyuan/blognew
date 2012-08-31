63df6cfffded55df77fb596bc01b5962dbecdb7a
rickyuan <rushyzt@gmail.com>
2012-08-31 13:58:44 +0800

    add ga support

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :content, :category_id
   belongs_to :category
-  paginates_per 8
+  paginates_per 13
   default_scope order('created_at DESC')
 end
